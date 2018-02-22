86b907524296cf5e65ea0144c4076e50fa42e539
sapan <sapantanted99@gmail.com>
2018-02-22 10:48:35 +0530

    class moved to packages

--- src/main/java/Topic.java
+++
@@ -1,7 +0,0 @@
-public class Topic {
-String data_type = "data";
-String location="kresit";
-String sensor_type ="sch";
-String channel= "4";
-
-}
--- src/main/java/test.java
+++
@@ -1,5 +0,0 @@
-public class test {
-	public static void main(String[] args) {
-		System.out.println((long)UtilsHandler.tsInSeconds(2016,10,1,0,0,0)); //Timestamp of 2016/10/1
-	}
-}
