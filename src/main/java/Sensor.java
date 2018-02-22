50c43d2e0f17ea82c2c11d632558135127ff9f3e
sapan <sapantanted99@gmail.com>
2018-02-22 10:45:45 +0530

    temp

--- src/main/java/Sensor.java
+++ src/main/java/Sensor.java
@@ -8,20 +8,42 @@ public class Sensor {
     String toTableName;
     float startTS;
     MySQLHandler mySQLHandler;
+    String timeField;
 
     public Sensor(String fromTableName, String sensorId, String toTableName) {
         this.fromTableName = fromTableName;
         this.sensorId = sensorId;
         this.toTableName = toTableName;
+        this.timeField = "ts";
         mySQLHandler = new MySQLHandler(ConfigHandler.MYSQL_HOST, ConfigHandler.MYSQL_USERNAME, ConfigHandler.MYSQL_PASSWORD, ConfigHandler.MYSQL_DATABASE_NAME);
+        fetchStartTimestamp();
+
     }
 
-    public void fetchStartTS() {
+	public String getTimeField() {
+		return timeField;
+	}
+
+	public void setTimeField(String timeField) {
+		this.timeField = timeField;
+	}
+
+	public void fetchStartTimestamp() {
         try {
-            String query = "select ts from " + toTableName + " order by ts desc limit 1";
-            ResultSet resultSet = mySQLHandler.query(query);
+            String sql = "select "+timeField+" from " + toTableName + " order by "+this.timeField+" desc limit 1";
+            ResultSet resultSet = mySQLHandler.query(sql);
             if (resultSet.next()) {
-                this.startTS = resultSet.getFloat("ts");
+                this.startTS = resultSet.getFloat(timeField);
+            }else{
+            	resultSet.close();
+            	//toTableName is empty table then fetch first ts from fromTableName
+	            sql ="select "+timeField+" from " + toTableName + " order by "+this.timeField+" asc limit 1";
+	            resultSet=mySQLHandler.query(sql);
+	            if(resultSet.next()){
+	            	this.startTS = resultSet.getFloat(timeField);
+	            }else{
+					this.startTS = (float)UtilsHandler.tsInSeconds(2016,10,1,0,0,0); //Timestamp of 2016/10/1
+	            }
             }
         } catch (SQLException e) {
             LogHandler.logError("[MySQL][Query]" + e.getMessage());
