4dce02e47b3181e3e3899e72e74ccaa2cf9e0738
sapan <sapantanted99@gmail.com>
2018-02-25 23:14:55 +0530

    aggregation done...code quality need to be improved

--- src/main/java/main/Sensor.java
+++ src/main/java/main/Sensor.java
@@ -1,126 +1,151 @@
 package main;
 
+import org.apache.spark.sql.DataFrameWriter;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashMap;
-import java.util.Properties;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
 
 public class Sensor {
 
-    String fromTableName;
-    String sensorId;
-    String toTableName;
-    double startTS;
-    MySQLHandler mySQLHandler;
-    String timeField;
-    Spark spark;
+	String fromTableName;
+	String sensorId;
+	String toTableName;
+	double startTS;
+	MySQLHandler mySQLHandler;
+	String timeField;
+	Spark spark;
 
-    public Sensor(String fromTableName, String sensorId, String toTableName) {
-        this.fromTableName = fromTableName;
-        this.sensorId = sensorId;
-        this.toTableName = toTableName;
-        this.timeField = "ts";
-        this.startTS = -1;
-        mySQLHandler = new MySQLHandler(ConfigHandler.MYSQL_HOST, ConfigHandler.MYSQL_USERNAME, ConfigHandler.MYSQL_PASSWORD, ConfigHandler.MYSQL_DATABASE_NAME);
-        this.spark = new Spark();
-        fetchStartTimestamp();
+	public Sensor(String fromTableName, String sensorId, String toTableName) {
+		this.fromTableName = fromTableName;
+		this.sensorId = sensorId;
+		this.toTableName = toTableName;
+		this.timeField = "ts";
+		this.startTS = -1;
+		mySQLHandler = new MySQLHandler(ConfigHandler.MYSQL_HOST, ConfigHandler.MYSQL_USERNAME, ConfigHandler.MYSQL_PASSWORD, ConfigHandler.MYSQL_DATABASE_NAME);
+		this.spark = new Spark();
+		fetchStartTimestamp();
+//		this.startTS = UtilsHandler.tsInSeconds(2017, 10, 3, 0, 0, 0);//base timestamp
 
-    }
+	}
 
-    public void fetchStartTimestamp() {
-        try {
-            String sql = "select " + timeField + " from " + toTableName + " order by " + this.timeField + " desc limit 1";
-            ResultSet resultSet = mySQLHandler.query(sql);
-            if (resultSet.next()) {
-                this.startTS = resultSet.getDouble(timeField) + 1; //+1 because we want to aggregate from the next second
-            } else {
-                resultSet.close();
-                //toTableName is empty table then fetch first ts from fromTableName
-                sql = "select " + timeField + " from " + fromTableName + " order by " + this.timeField + " asc limit 1";
-                resultSet = mySQLHandler.query(sql);
-                if (resultSet.next()) {
-                    this.startTS = resultSet.getDouble(timeField); //No +1 because we want to aggregate from this second itself
-                } else {
-                    this.startTS = UtilsHandler.tsInSeconds(2016, 10, 1, 0, 0, 0);//base timestamp
-                }
-            }
-            this.startTS = this.startTS - this.startTS % ConfigHandler.AGGREGATION_RANGE_IN_SECONDS;
-        } catch (SQLException e) {
-            LogHandler.logError("[MySQL][Query][SensorClass]" + e.getMessage());
-            UtilsHandler.exit_thread();
-        }
-    }
+	public void fetchStartTimestamp() {
+		try {
+			String sql = "select " + timeField + " from " + toTableName + " order by " + this.timeField + " desc limit 1";
+			ResultSet resultSet = mySQLHandler.query(sql);
+			if (resultSet.next()) {
+				this.startTS = resultSet.getDouble(timeField) + 1; //+1 because we want to aggregate from the next second
+			} else {
+				resultSet.close();
+				//toTableName is empty table then fetch first ts from fromTableName
+				sql = "select " + timeField + " from " + fromTableName + " order by " + this.timeField + " asc limit 1";
+				resultSet = mySQLHandler.query(sql);
+				if (resultSet.next()) {
+					this.startTS = resultSet.getDouble(timeField); //No +1 because we want to aggregate from this second itself
+				} else {
+					this.startTS = UtilsHandler.tsInSeconds(2016, 10, 1, 0, 0, 0);//base timestamp
+				}
+			}
+			this.startTS = this.startTS - this.startTS % ConfigHandler.AGGREGATION_RANGE_IN_SECONDS;
+		} catch (SQLException e) {
+			LogHandler.logError("[MySQL][Query][SensorClass]" + e.getMessage());
+			UtilsHandler.exit_thread();
+		}
+	}
 
 
-    public void startArchivalAggregation() {
+	public void startArchivalAggregation() {
         /*
         This function starts aggregation of archival data after the last aggregated row present corresponding to this sensor
          */
-        Dataset<Row> rows = fetchDataForAggregation();
-        rows.show();
-        rows = aggregateData(rows);
-        rows.show();
-        storeAggregatedData(rows);
-    }
+        this.fetchStartTimestamp();
+		LogHandler.logInfo("["+sensorId+"]aggregation started for minute "+UtilsHandler.tsToStr(this.startTS));
+		Dataset<Row> rows = fetchDataForAggregation();
+		rows = aggregateData(rows);
+		storeAggregatedData(rows);
+	}
 
-    private Dataset<Row> aggregateData(Dataset<Row> rows) {
-        HashMap<String,String> aggregationMap = getAggregationMap(fromTableName);
-        rows = rows.agg(aggregationMap);
-        rows.withColumnRenamed("avg(W)","W");
-        return rows;
-    }
+	private Dataset<Row> aggregateData(Dataset<Row> rows) {
+		HashMap<String, String> aggregationMap = getAggregationMap(fromTableName);
+		rows.createOrReplaceTempView("sensor_data");
+		String sql = "select";
+		Set<String> keySet = aggregationMap.keySet();
+		for (Iterator<String> iterator = keySet.iterator(); iterator.hasNext(); ) {
+			String key = iterator.next();
+			String value = aggregationMap.get(key);
+			String valueAsKey = " " + value + " as " + key + ",";
+			sql = sql + valueAsKey;
+		}
+		sql = sql.substring(0, sql.length() - 1);
+		sql = sql + " from sensor_data";
+//		LogHandler.logInfo("[AggregationQuery]" + sql);
+		rows = spark.sparkSession.sql(sql);
+		return rows;
+	}
 
-    private HashMap<String, String> getAggregationMap(String tableName) {
-        HashMap<String,String> aggregationMap = new HashMap<String, String>();
-        aggregationMap.put("W","avg");
-        aggregationMap.put("W1","avg");
-        aggregationMap.put("W2","avg");
-        aggregationMap.put("W3","avg");
-        aggregationMap.put("V1","avg");
-        aggregationMap.put("V2","avg");
-        aggregationMap.put("V3","avg");
-        aggregationMap.put("A","avg");
-        aggregationMap.put("A1","avg");
-        aggregationMap.put("A2","avg");
-        aggregationMap.put("A3","avg");
-        return aggregationMap;
-    }
+	private HashMap<String, String> getAggregationMap(String tableName) {
+		HashMap<String, String> aggregationMap = new HashMap<String, String>();
+		aggregationMap.put("sensor_id", "first(sensor_id)");
+		aggregationMap.put("TS_RECV", "last(TS_RECV)");
+		aggregationMap.put("TS", (startTS + ConfigHandler.AGGREGATION_RANGE_IN_SECONDS)+"");
+		aggregationMap.put("W", "avg(W)");
+		aggregationMap.put("W1", "avg(W1)");
+		aggregationMap.put("W2", "avg(W2)");
+		aggregationMap.put("W3", "avg(W3)");
+		aggregationMap.put("V1", "avg(V1)");
+		aggregationMap.put("V2", "avg(V2)");
+		aggregationMap.put("V3", "avg(V3)");
+		aggregationMap.put("A", "avg(A)");
+		aggregationMap.put("A1", "avg(A1)");
+		aggregationMap.put("A2", "avg(A2)");
+		aggregationMap.put("A3", "avg(A3)");
+		aggregationMap.put("VA", "avg(VA)");
+		aggregationMap.put("VA1", "avg(VA1)");
+		aggregationMap.put("VA2", "avg(VA2)");
+		aggregationMap.put("VA3", "avg(VA3)");
+		aggregationMap.put("PF", "avg(W)/avg(VA)");
+		aggregationMap.put("PF1", "avg(W1)/avg(VA1)");
+		aggregationMap.put("PF2", "avg(W2)/avg(VA2)");
+		aggregationMap.put("PF3", "avg(W3)/avg(VA3)");
+		aggregationMap.put("FwdWh", "last(FwdWh)");
+		aggregationMap.put("delta_FwdWh", "last(FwdWh)-first(FwdWh)");
+		aggregationMap.put("data_percent"
+				, "count(*)/"+(ConfigHandler.AGGREGATION_RANGE_IN_SECONDS/ConfigHandler.RATE_OF_DATA_PER_SECOND)+"*100");
+		return aggregationMap;
+	}
 
-    private void storeAggregatedData(Dataset<Row> rows) {
+	private void storeAggregatedData(Dataset<Row> rows) {
+		DataFrameWriter<Row> df =  new DataFrameWriter<Row>(rows);
+		df.mode("append").jdbc(mySQLHandler.url,toTableName,spark.getProperties());
+	}
 
-    }
-    private Dataset<Row> fetchDataForAggregation() {
-        Dataset<Row> rows = getRows(fromTableName);
-        rows = rows.where("sensor_id="+"'"+sensorId+"' and "+
-                timeField + " >= " + startTS + " and " + timeField + " < " + (startTS + ConfigHandler.AGGREGATION_RANGE_IN_SECONDS));
-        return rows;
-    }
+	private Dataset<Row> fetchDataForAggregation() {
+		Dataset<Row> rows = spark.getRowsByTableName(fromTableName);
+		rows = rows.where("sensor_id=" + "'" + sensorId + "' and " +
+				timeField + " >= " + startTS + " and " + timeField + " < " + (startTS + ConfigHandler.AGGREGATION_RANGE_IN_SECONDS));
+		rows = rows.sort(timeField);
+		return rows;
+	}
 
-    public String getTimeField() {
-        return timeField;
-    }
+	public String getTimeField() {
+		return timeField;
+	}
 
-    public void setTimeField(String timeField) {
-        this.timeField = timeField;
-    }
+	public void setTimeField(String timeField) {
+		this.timeField = timeField;
+	}
 
-    public double getStartTS() {
-        return startTS;
-    }
+	public double getStartTS() {
+		return startTS;
+	}
 
-    public void setStartTS(double startTS) {
-        this.startTS = startTS;
-    }
+	public void setStartTS(double startTS) {
+		this.startTS = startTS;
+	}
 
-
-    private Dataset<Row> getRows(String tableName){
-        Properties properties = new Properties();
-        properties.setProperty("user", mySQLHandler.username);
-        properties.setProperty("password", mySQLHandler.password);
-        Dataset<Row> rows = spark.sparkSession.read().jdbc(mySQLHandler.url, tableName, properties);
-        return  rows;
-    }
 }
--- src/main/java/main/Spark.java
+++ src/main/java/main/Spark.java
@@ -2,7 +2,11 @@ package main;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+
+import java.util.Properties;
 
 public class Spark {
     SparkSession sparkSession;
@@ -24,5 +28,19 @@ public class Spark {
     public void logsOff(){
         Logger.getLogger("org").setLevel(Level.OFF);
         Logger.getLogger("akka").setLevel(Level.OFF);
+    }
+
+
+    public Properties getProperties(){
+        Properties properties = new Properties();
+        properties.setProperty("user", ConfigHandler.MYSQL_USERNAME);
+        properties.setProperty("password", ConfigHandler.MYSQL_PASSWORD);
+        return properties;
+    }
+    public  Dataset<Row> getRowsByTableName(String tableName){
+        Properties properties =getProperties();
+        String url  = "jdbc:mysql://" + ConfigHandler.MYSQL_HOST+ ":3306/"+ConfigHandler.MYSQL_DATABASE_NAME+"?useSSL=false";
+        Dataset<Row> rows = this.sparkSession.read().jdbc(url, tableName, properties);
+        return  rows;
     }
 }
