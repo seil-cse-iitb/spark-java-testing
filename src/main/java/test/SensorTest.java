c8c70dd510b21426f8cb6d94e21b01e705ec44d4
sapan <sapantanted99@gmail.com>
2018-03-01 17:38:26 +0530

    temporary commit

--- src/main/java/test/SensorTest.java
+++ src/main/java/test/SensorTest.java
@@ -1,31 +1,26 @@
 package test;
 
-import main.Sensor;
+import main.SensorAggregation;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
-import org.apache.spark.sql.types.StructField;
 import org.apache.spark.sql.types.StructType;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.apache.spark.sql.types.DataTypes.DoubleType;
-import static org.apache.spark.sql.types.DataTypes.IntegerType;
-import static org.apache.spark.sql.types.DataTypes.StringType;
-
 public class SensorTest {
-    Sensor sensor ;
+    SensorAggregation sensorAggregation;
     @Before
     public void init(){
-        sensor = new Sensor("sch_3","power_k_sr_a","agg_sch_3");
+        sensorAggregation = new SensorAggregation("sch_3","power_k_sr_a","agg_sch_3");
     }
     @Test
     public void fetchStartTimestampTest(){
-        Assert.assertNotEquals(-1,sensor.getStartTS());
+        Assert.assertNotEquals(-1, sensorAggregation.getStartTS());
     }
     @Test
     public void schemaTest(){
-        Dataset<Row> rowDataset = sensor.fetchDataForAggregation();
+        Dataset<Row> rowDataset = sensorAggregation.fetchDataForAggregation();
         StructType schema = rowDataset.schema();
         System.out.println(schema);
     }
---
+++ src/main/java/test/TableAggregationTest.java
@@ -0,0 +1,30 @@
+package test;
+
+import main.LogHandler;
+import main.SensorAggregation;
+import main.TableAggregation;
+
+public class TableAggregationTest {
+	public static void main(String[] args) {
+			Thread thread = new Thread(new Runnable() {
+				public void run() {
+					String fromTableName = "sch_3";
+					String toTableName = "agg_sch_3";
+					try {
+
+						LogHandler.logInfo("[Thread][Start] started for table:"+fromTableName );
+						TableAggregation tableAggregation  = new TableAggregation(fromTableName,toTableName);
+						int i = 1051200;
+						while (i > 0) {
+							tableAggregation.startArchivalAggregation();
+							i--;
+						}
+						LogHandler.logInfo("[Thread][End] ended for table:"+fromTableName);
+					} catch (Exception e) {
+						LogHandler.logError("From table:[" + fromTableName + "] To table:[" + toTableName + "]" + e.getMessage());
+					}
+				}
+			});
+			thread.start();
+	}
+}
